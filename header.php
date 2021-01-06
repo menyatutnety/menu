ef416f8238bfb2458dd5fbaa19126518ecae2619
menyatutnety <74410159+menyatutnety@users.noreply.github.com>
2021-01-06 13:50:09 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Uckan.Net - Responsive Açılır Kapanır Menu</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="toggle"><span class="menu-title">Menu</span> <i class="fa fa-list"></i></span></div>
+	<nav id="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+        <ul class="menu">
+            <li><a href="#">Anasayfa</a></li>
+            <li><a href="#">Hakkında</a></li>
+            <li><a href="#">S.S.S</a></li>
+            <li><a href="#">Kullanım Sözleşmesi</a></li>
+            <li><a href="#">Bize Ulaşın</a></li>
+            <li><a href="#">Künye</a></li>
+        </ul>
+	</nav>
+	
+	<p> Responsive açılır kapanır menü örneği - <a href="http://www.uckan.net">Uckan.Net</a></p>
+  
+</div>
+  
+  <!-- Portfolio--><a id="portfolio" href="http://www.uckan.net" title="Sitemi ziyaret edebilirsin"><i class="fa fa-link"></i></a>
+<!-- CodePen--><a id="codepen" href="https://codepen.io/uckannet/" title="Takip Edebilirsin"><i class="fa fa-codepen"></i></a>
+
+</body>
+</html>
